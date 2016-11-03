0cd6b93351c354d08cc801e524cba46400ad4989
Dafa Zakhulhaq <dafa27890@gmail.com>
2016-11-03 08:39:09 +0700

    RcyclerView List Card

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -1,15 +1,30 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter;
+import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ArrayList<Hotel> mlist = new ArrayList<>();
+    HotelAdapter madapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,41 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        madapter = new HotelAdapter(mlist);
+        recyclerView.setAdapter(madapter);
+
+        filldata();
+
+    }
+
+    private void filldata() {
+
+        Resources resources = getResources();
+        String[] arjudul = resources.getStringArray(R.array.places);
+        String[] ardeskripsi = resources.getStringArray(R.array.place_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+        Drawable[] arfoto = new Drawable[a.length()];
+
+        for (int i = 0; i < arfoto.length; i++) {
+
+            arfoto[i] = a.getDrawable(i);
+
+        }
+        a.recycle();
+
+
+        for (int i = 0; i < arjudul.length; i++) {
+            mlist.add(new Hotel(arjudul[i], ardeskripsi[i], arfoto[i]));
+
+        }
+        madapter.notifyDataSetChanged();
+
+
     }
 
     @Override
